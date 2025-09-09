acea10de01dc592df61771ea60bd444297ac40f9
Swxblet <gerasolanod21@gmail.com>
2025-09-08 21:35:58 -0600

    Versión Beta, clase menú creada, falta trabajar en ella

---
+++ Proyecto/src/Menu.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Menu {
+    public Menu() {
+        int option;
+        JOptionPane.showMessageDialog(null, """
+                Bienvenido a la calculadora de préstamos\s
+                1. Agregar Perfil de préstamo\s
+                2. Editar Perfil de préstamo\s
+                3. Eliminar Perfil de préstamo\s
+                4. Calcular Préstamo\s
+                5. Salir""");
+                //POR TRABAJAR PORQUE ME BLOQUEE, FALTA CAMBIAR EL JOptionPane por algo que admita cambiar el texto de la caja y que admita un campo de texto
+    }
+}
