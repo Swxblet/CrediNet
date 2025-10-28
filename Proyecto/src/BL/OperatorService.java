99f2cdaf5fdbe752edba5618566a45edbb6d5e84
Swxblet <gerasolanod21@gmail.com>
2025-10-27 22:39:40 -0600

    OperatorService trabajado, muchas cosas por hacer

--- Proyecto/src/BL/OperatorService.java
+++ Proyecto/src/BL/OperatorService.java
@@ -1,4 +1,20 @@
 package BL;
 
+import DAL.OperatorDAL;
+import ET.Operator;
+
 public class OperatorService {
+    OperatorDAL operatorDAL = new OperatorDAL();
+    public OperatorService() throws Exception {
+
+    }
+    public Operator searchOperator(int id){
+        for(Operator eachOperator : operatorDAL.getOperators()){
+            if(eachOperator.getOperatorId() == id){
+                return eachOperator;
+            }
+        }
+        return null;
+    }
+    //TODO
 }
