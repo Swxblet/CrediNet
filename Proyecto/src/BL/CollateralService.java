7682ba2282d82dd8b0f67a1042fad62082d52226
Swxblet <gerasolanod21@gmail.com>
2025-10-29 15:52:42 -0600

    Avances hechos en CollateralService

--- Proyecto/src/BL/CollateralService.java
+++ Proyecto/src/BL/CollateralService.java
@@ -1,4 +1,28 @@
 package BL;
 
+import DAL.CollateralDAL;
+import ET.Collateral;
+
 public class CollateralService {
+    CollateralDAL collateralDAL = new CollateralDAL();
+    public CollateralService() throws Exception {
+
+    }
+    public Collateral searchCollateral(int id){
+        for(Collateral eachCollateral : collateralDAL.getCollaterals()){
+            if(eachCollateral.getCollateralId() == id){
+                return eachCollateral;
+            }
+        }
+        return null;
+    }
+    public void addCollateral(Collateral collateralToAdd) throws Exception{
+        collateralDAL.collateralInsert(collateralToAdd);
+    }
+    public void deleteCollateral(Collateral collateralToDelete) throws Exception{
+        collateralDAL.collateralDelete(collateralToDelete);
+    }
+    public void updateCollateral(Collateral collateralToUpdate) throws Exception{
+        collateralDAL.collateralUpdate(collateralToUpdate);
+    }
 }
